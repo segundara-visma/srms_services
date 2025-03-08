9d1a5990927dd0db1d6de6fc03f86e45c2ed9449
Olusegun Okedara <olusegun.okedara@visma.com>
2025-03-08 13:50:27 +0200

    feat: Added UserService basic structure

---
+++ services/UserService/tests/UserService.UnitTests/UserServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using Moq;
+using UserService.Application.Services;
+using UserService.Domain.Entities;
+using UserService.Application.Interfaces;
+using System.Threading.Tasks;
+
+public class UserServiceTests
+{
+    [Fact]
+    public async Task RegisterUser_ShouldCreateUser_WhenDataIsValid()
+    {
+        // Arrange
+        var userRepositoryMock = new Mock<IUserRepository>();
+        var userService = new UserService(userRepositoryMock.Object);
+
+        var user = new User("test@example.com", "hashedpassword");
+        userRepositoryMock.Setup(repo => repo.CreateAsync(user)).ReturnsAsync(user);
+
+        // Act
+        var result = await userService.RegisterUser(user);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("test@example.com", result.Email);
+    }
+}
